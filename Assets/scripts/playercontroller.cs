bf1f06c1a83f30b57b62e317baf19258681b327b
cameronhuntley-star <dani.emigh@discoverystudent.org>
2025-11-03 10:40:12 -0500

    11/2

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
-using unityengine.InputSystem;
+using UnityEngine.InputSystem;
 
 public class playercontroller : MonoBehaviour
 {
+
+    private Rigidbody rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -15,5 +18,12 @@ public class playercontroller : MonoBehaviour
         
     }
 
-    
+   void OnMove(InputValue movementValue){
+    Vector2 movementVector = movementValue.Get<Vector2>();
+   }
+
+    private void FixedUpdate(){
+        rb.AddForce(movement);
+    }
+
 }
