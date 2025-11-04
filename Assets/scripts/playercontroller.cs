af40468cfaab313155ce4ce145bff5050b80a4e0
kaleb <kaleb.brown@discoverystudent.org>
2025-11-04 11:33:18 -0500

    11/

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -6,6 +6,11 @@ public class playercontroller : MonoBehaviour
 
     private Rigidbody rb;
 
+    private float movementX;
+    private float movementY;
+
+    public float speed = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,10 +25,16 @@ public class playercontroller : MonoBehaviour
 
    void OnMove(InputValue movementValue){
     Vector2 movementVector = movementValue.Get<Vector2>();
+
+    movementX = movementVector.x;
+    movementY = movementVector.y;
+    
    }
 
     private void FixedUpdate(){
-        rb.AddForce(movement);
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement *speed);
     }
 
 }
